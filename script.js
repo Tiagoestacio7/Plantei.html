19768d6decc59b1a3df0e8441b90b62acef8498c
unknown <tiagosilva0547@digitalcollege.internal>
2026-02-05 20:55:47 -0300

    Implemetação de opção de ternário

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function togglePassword(){
     const inputPassword = document.getElementById('password')
     const eyeIcon = document.getElementById('eyeIcon')
 
-    if (inputPassword.type === 'password'){
+    // Utilização de if/else
+    /*if (inputPassword.type === 'password'){
         inputPassword.type = 'text'
         eyeIcon.classList.remove('bi-eye')
         eyeIcon.classList.add('bi-eye-slash')
@@ -13,7 +14,14 @@ function togglePassword(){
         inputPassword.type = 'password'
         eyeIcon.classList.add('bi-eye')
         eyeIcon.classList.remove('bi-eye-slash')
+    }*/
 
-    }
+    // Utilização com ternário
+    const isPassword = inputPassword.type === 'password';
+    
+    inputPassword.type = isPassword ? 'text' : 'password'
+    eyeIcon.classList.remove(isPassword ? 'bi-eye' : 'bi-eye-slash')
+    eyeIcon.classList.add(isPassword ? 'bi-eye-slash' : 'bi-eye')    
+ 
 
 }
